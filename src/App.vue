3287f19bce31989befeaed91928a2347bae972d9
suixin <suixin0309@163.com>
2022-03-24 15:30:52 +0800

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Xin msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import Xin from './components/xin.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Xin
+  }
+}
+</script>
