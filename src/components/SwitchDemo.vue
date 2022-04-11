5bae094db5b06463553db1d1c40c79c070b5cdad
suixin <suixin0309@163.com>
2022-04-11 16:29:49 +0800

    switch 接受value和input时间

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <SwitchDe />
+        <SwitchDe :value="y" @input="y = $event"/>
     </div>
 </template>
 <script lang="ts">
+import { ref } from "vue";
 import SwitchDe from "../lib/SwitchDe.vue";
 export default{
-    components: { SwitchDe }
+    components: { SwitchDe },
+    setup() {
+        const y = ref(false);
+        return { y }
+    }
 }
 </script>
